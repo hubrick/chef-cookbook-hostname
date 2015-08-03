463b244732b3be11f9a23f27a178925929437c6d
Dimitris Verraros <vervas.g@gmail.com>
2015-08-03 16:17:06 +0200

    Set flag for managing the hostsfile
    Make hostsfile changes optional in case of manual handling of it in
    other places.

--- recipes/default.rb
+++ recipes/default.rb
@@ -112,20 +112,22 @@ if fqdn
     end
   end
 
-  hostsfile_entry 'localhost' do
-    ip_address '127.0.0.1'
-    hostname 'localhost'
-    action :append
-  end
+  if node['hostname_cookbook']['manage_hostfile']
+    hostsfile_entry 'localhost' do
+      ip_address '127.0.0.1'
+      hostname 'localhost'
+      action :append
+    end
 
-  hostsfile_entry 'set hostname' do
-    ip_address node['hostname_cookbook']['hostsfile_ip']
-    hostname fqdn
-    aliases aliases
-    unique true
-    action :create
-    notifies :reload, 'ohai[reload_hostname]', :immediately
-    only_if { node['hostname_cookbook']['append_hostsfile_ip'] }
+    hostsfile_entry 'set hostname' do
+      ip_address node['hostname_cookbook']['hostsfile_ip']
+      hostname fqdn
+      aliases aliases
+      unique true
+      action :create
+      notifies :reload, 'ohai[reload_hostname]', :immediately
+      only_if { node['hostname_cookbook']['append_hostsfile_ip'] }
+    end
   end
 
   ohai 'reload_hostname' do
--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -93,4 +93,36 @@ describe 'hostname::default' do
 
     expect(chef_run).to_not create_hostsfile_entry('set hostname')
   end
+
+  it 'appends localhost hostfile entry when manage_hostfile is true' do
+    chef_run.node.set['set_fqdn'] = 'test.example.com'
+    chef_run.node.set['hostname_cookbook']['manage_hostfile'] = true
+    chef_run.converge 'hostname'
+
+    expect(chef_run).to append_hostsfile_entry('localhost')
+  end
+
+  it 'does not append localhost hostfile entry when manage_hostfile is false' do
+    chef_run.node.set['set_fqdn'] = 'test.example.com'
+    chef_run.node.set['hostname_cookbook']['manage_hostfile'] = false
+    chef_run.converge 'hostname'
+
+    expect(chef_run).to_not append_hostsfile_entry('localhost')
+  end
+
+  it 'creates hostname hostfile entry when manage_hostfile is true' do
+    chef_run.node.set['set_fqdn'] = 'test.example.com'
+    chef_run.node.set['hostname_cookbook']['manage_hostfile'] = true
+    chef_run.converge 'hostname'
+
+    expect(chef_run).to create_hostsfile_entry('set hostname')
+  end
+
+  it 'does not create hostname hostfile entry when manage_hostfile is false' do
+    chef_run.node.set['set_fqdn'] = 'test.example.com'
+    chef_run.node.set['hostname_cookbook']['manage_hostfile'] = false
+    chef_run.converge 'hostname'
+
+    expect(chef_run).to_not create_hostsfile_entry('set hostname')
+  end
 end
